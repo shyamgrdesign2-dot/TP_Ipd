e531581bb218dd3d983fe79c4140f519f3d8d282
JW <jaydeep.w@gmail.com>
2023-12-07 18:12:58 +0530

    Implement show/hide

--- src/components/AddressDetails.js
+++ src/components/AddressDetails.js
@@ -7,6 +7,7 @@ import { searchPincode } from "../redux/appointmentsSlice";
 
 function AddressDetails({ patientInfo, setPatientInfo }) {
   const dispatch = useDispatch();
+  const [showDetails, setShowDetails] = useState(true);
   let { pincodeInfo, error } = useSelector((state) => state.records);
 
   console.log("pincodeInfo: ", pincodeInfo);
@@ -55,50 +56,70 @@ function AddressDetails({ patientInfo, setPatientInfo }) {
     <>
       <div className="d-flex justify-content-between">
         <div className="title">Address Details</div>
-        <Button className="border-0 shadow-none">
+        <Button
+          className="border-0 shadow-none"
+          onClick={() => {
+            setShowDetails(!showDetails);
+          }}
+        >
           <div className="title align-items-center d-flex">
             {" "}
-            <i className="icon-Add me-2"></i> Add Details
+            {showDetails ? (
+              <>
+                <i className="icon-minus me-2" /> Show Less
+              </>
+            ) : (
+              <>
+                <i className="icon-Add me-2" /> Add Details
+              </>
+            )}
           </div>
         </Button>
       </div>
-      <Form
-        layout="vertical"
-        name="advanced_search"
-        className="form_addnewpatient"
-      >
-        <Row className="mt-3" gutter={{ xs: 8, sm: 18, md: 40, lg: 94 }}>
-          <Col xs={24} sm={24} md={12} lg={12}>
-            <Form.Item name="Pincode" label="Pincode">
-              <Input placeholder="Enter Pin Code" type="number" maxLength={6} onChange={onSearch} />
-            </Form.Item>
-          </Col>
-          <Col xs={24} sm={24} md={12} lg={12}>
-            <Form.Item name="city" label="City">
-              <Input placeholder={pincodeInfo?.city ?? "City"} disabled />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row className="mt-3" gutter={{ xs: 8, sm: 18, md: 40, lg: 94 }}>
-          <Col xs={24} sm={24} md={12} lg={12}>
-            <Form.Item name="state" label="State">
-              <Select
-                placeholder={pincodeInfo?.state ?? "Select state"}
-                disabled
-              />
-            </Form.Item>
-          </Col>
-          <Col xs={24} sm={24} md={12} lg={12}>
-            <Form.Item name="streetaddress" label="Street Address">
-              <Input
-                placeholder="Address"
-                id="pm_address"
-                onChange={onFieldChanged}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
+      {showDetails && (
+        <Form
+          layout="vertical"
+          name="advanced_search"
+          className="form_addnewpatient"
+        >
+          <Row className="mt-3" gutter={{ xs: 8, sm: 18, md: 40, lg: 94 }}>
+            <Col xs={24} sm={24} md={12} lg={12}>
+              <Form.Item name="Pincode" label="Pincode">
+                <Input
+                  placeholder="Enter Pin Code"
+                  type="number"
+                  maxLength={6}
+                  onChange={onSearch}
+                />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12} lg={12}>
+              <Form.Item name="city" label="City">
+                <Input placeholder={pincodeInfo?.city ?? "City"} disabled />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row className="mt-3" gutter={{ xs: 8, sm: 18, md: 40, lg: 94 }}>
+            <Col xs={24} sm={24} md={12} lg={12}>
+              <Form.Item name="state" label="State">
+                <Select
+                  placeholder={pincodeInfo?.state ?? "Select state"}
+                  disabled
+                />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12} lg={12}>
+              <Form.Item name="streetaddress" label="Street Address">
+                <Input
+                  placeholder="Address"
+                  id="pm_address"
+                  onChange={onFieldChanged}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      )}
     </>
   );
 }
--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 
 import { Button, Form, Input, Select, DatePicker, Radio, Row, Col } from "antd";
-import { calculateAge, calculateBirthdateFromAge, getFormattedDate } from "../utils/utils";
+import {
+  calculateAge,
+  calculateBirthdateFromAge,
+  getFormattedDate,
+} from "../utils/utils";
 import dayjs from "dayjs";
 
 function PersonalDetails({ patientInfo, setPatientInfo }) {
   const [form] = Form.useForm();
-    const [ageYearsMonths, setAgeYearsMonths] = useState(null);
-    const [birthDate, setBirthDate] = useState(null);
+  const [showDetails, setShowDetails] = useState(true);
+  const [ageYearsMonths, setAgeYearsMonths] = useState(null);
+  const [birthDate, setBirthDate] = useState(null);
 
   // Select for Salutation
   const salutationOption = [
@@ -32,7 +37,7 @@ function PersonalDetails({ patientInfo, setPatientInfo }) {
   };
 
   const onBirthDateChanged = (date, dateString) => {
-    console.log('onBirthDateChanged triggred: ');
+    console.log("onBirthDateChanged triggred: ");
     console.log(date, dateString);
     const age = calculateAge(getFormattedDate(dateString));
     setAgeYearsMonths(age);
@@ -42,32 +47,37 @@ function PersonalDetails({ patientInfo, setPatientInfo }) {
     });
   };
 
-/*   useEffect(() => {
-    if(ageYearsMonths?.years && ageYearsMonths?.months) {
-      const birthDate = calculateBirthdateFromAge(ageYearsMonths);
-      console.log('ageYearsMonths: mutated birthDate: ', birthDate);
-      setBirthDate(birthDate);
-      setPatientInfo({
-        ...patientInfo,
-        pm_dob: getFormattedDate(birthDate),
-      });
-    }
-  }, [ageYearsMonths]); */
-
   useEffect(() => {
     if (ageYearsMonths) {
       let dateObj = new Date();
-      let year = (dateObj.getUTCFullYear()) - parseInt(ageYearsMonths.hasOwnProperty('years') ? ageYearsMonths.years != '' ? ageYearsMonths.years : 0 : 0);
-      let month = (dateObj.getUTCMonth() + 1) - parseInt(ageYearsMonths.hasOwnProperty('months') ? ageYearsMonths.months != '' ? ageYearsMonths.months : 0 : 0); //months from 1-12
-      let day = (dateObj.getUTCDate()) - 0;
+      let year =
+        dateObj.getUTCFullYear() -
+        parseInt(
+          ageYearsMonths.hasOwnProperty("years")
+            ? ageYearsMonths.years != ""
+              ? ageYearsMonths.years
+              : 0
+            : 0
+        );
+      let month =
+        dateObj.getUTCMonth() +
+        1 -
+        parseInt(
+          ageYearsMonths.hasOwnProperty("months")
+            ? ageYearsMonths.months != ""
+              ? ageYearsMonths.months
+              : 0
+            : 0
+        ); //months from 1-12
+      let day = dateObj.getUTCDate() - 0;
       let newdate = year + "-" + month + "-" + day;
-      console.log('newdate', newdate)
+      console.log("newdate", newdate);
       setPatientInfo({
         ...patientInfo,
         pm_dob: getFormattedDate(newdate),
       });
       form.setFieldsValue({
-        dateofbirth: dayjs(getFormattedDate(newdate), "YYYY-MM-DD")
+        dateofbirth: dayjs(getFormattedDate(newdate), "YYYY-MM-DD"),
       });
     }
   }, [ageYearsMonths]);
@@ -112,100 +122,138 @@ function PersonalDetails({ patientInfo, setPatientInfo }) {
     <>
       <div className="d-flex justify-content-between">
         <div className="title">Personal Details</div>
-        <Button className="border-0 shadow-none">
+        <Button
+          className="border-0 shadow-none"
+          onClick={() => {
+            setShowDetails(!showDetails);
+          }}
+        >
           <div className="title align-items-center d-flex">
             {" "}
-            <i className="icon-Add me-2"></i> Add Details
+            {showDetails ? (
+              <>
+                <i className="icon-minus me-2" /> Show Less
+              </>
+            ) : (
+              <>
+                <i className="icon-Add me-2" /> Add Details
+              </>
+            )}
           </div>
         </Button>
       </div>
 
-      <Form
-        form={form}
-        layout="vertical"
-        name="advanced_search"
-        className="form_addnewpatient"
-      >
-        <Row gutter={{ xs: 8, sm: 18, md: 24, lg: 30 }}>
-          <Col xs={8} sm={8} md={6} lg={4}>
-            <Form.Item name="salutation" label="Salutation">
-              <Select
-                placeholder="Select"
-                options={salutationOption}
-                onChange={onSalutationChanged}
-              />
-            </Form.Item>
-          </Col>
-          <Col xs={16} sm={16} md={18} lg={20}>
-            <Form.Item name="fullname" label="Full Name" rules={rules.fullname}>
-              <Input
-                placeholder="Full Name"
-                id="pm_fullname"
-                onChange={onFieldChanged}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={{ xs: 8, sm: 18, md: 40, lg: 94 }}>
-          <Col xs={24} sm={24} md={12} lg={12}>
-            <Form.Item
-              name="mobilenumber"
-              label="Mobile Number"
-              rules={rules.mobilenumber}
-            >
-              <Input
-                placeholder="Enter 10 digit number"
-                id="pm_contact_no"
-                type="tel"
-                maxLength={10}
-                onChange={onFieldChanged}
-              />
-            </Form.Item>
-          </Col>
-          <Col xs={24} sm={24} md={12} lg={12}>
-            <Form.Item name="gender" label="Gender" rules={rules.gender}>
-              <Radio.Group onChange={onGenderChanged}>
-                <Radio.Button value="Male">Male</Radio.Button>
-                <Radio.Button value="Female">Female</Radio.Button>
-                <Radio.Button value="Other">Other</Radio.Button>
-              </Radio.Group>
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row className="align-items-center" gutter={{ xs: 0, sm: 0, lg: 0 }}>
-          <Col xs={24} sm={24} md={11} lg={11}>
-            <Form.Item
-              name="ageyearsmonths"
-              label="Age In Years & Months"
-              rules={rules.ageyearsmonths}
-            >
-              <div className="justify-content-between d-flex">
-                <Input className="w-48" type="number" placeholder="Years" value={ageYearsMonths?.years} onChange={(e) => {
-                  setAgeYearsMonths({
-                    ...ageYearsMonths,
-                    years: e.target.value
-                  });
-                }} />
-                <Input className="w-48" type="number" placeholder="Months" maxLength={2} value={ageYearsMonths?.months} onChange={(e) => {
-                  setAgeYearsMonths({
-                    ...ageYearsMonths,
-                    months: e.target.value
-                  });
-                }} />
-              </div>
-            </Form.Item>
-          </Col>
-          <Col xs={24} sm={24} md={2} lg={2}>
-            <div className="or text-center mt-2">OR</div>
-          </Col>
-          <Col xs={24} sm={24} md={11} lg={11}>
-            <Form.Item name="dateofbirth" label="Date of Birth">
-              <DatePicker onChange={onBirthDateChanged} value={birthDate ? dayjs(getFormattedDate(birthDate), 'YYYY-MM-DD') : null} />
-              {/* <DatePicker onChange={onBirthDateChanged} value={dayjs('1990-05-05', 'YYYY-MM-DD')} format = {'YYYY-MM-DD'} /> */}
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
+      {showDetails && (
+        <Form
+          form={form}
+          layout="vertical"
+          name="advanced_search"
+          className="form_addnewpatient"
+        >
+          <Row gutter={{ xs: 8, sm: 18, md: 24, lg: 30 }}>
+            <Col xs={8} sm={8} md={6} lg={4}>
+              <Form.Item name="salutation" label="Salutation">
+                <Select
+                  placeholder="Select"
+                  options={salutationOption}
+                  onChange={onSalutationChanged}
+                />
+              </Form.Item>
+            </Col>
+            <Col xs={16} sm={16} md={18} lg={20}>
+              <Form.Item
+                name="fullname"
+                label="Full Name"
+                rules={rules.fullname}
+              >
+                <Input
+                  placeholder="Full Name"
+                  id="pm_fullname"
+                  onChange={onFieldChanged}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={{ xs: 8, sm: 18, md: 40, lg: 94 }}>
+            <Col xs={24} sm={24} md={12} lg={12}>
+              <Form.Item
+                name="mobilenumber"
+                label="Mobile Number"
+                rules={rules.mobilenumber}
+              >
+                <Input
+                  placeholder="Enter 10 digit number"
+                  id="pm_contact_no"
+                  type="tel"
+                  maxLength={10}
+                  onChange={onFieldChanged}
+                />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12} lg={12}>
+              <Form.Item name="gender" label="Gender" rules={rules.gender}>
+                <Radio.Group onChange={onGenderChanged}>
+                  <Radio.Button value="Male">Male</Radio.Button>
+                  <Radio.Button value="Female">Female</Radio.Button>
+                  <Radio.Button value="Other">Other</Radio.Button>
+                </Radio.Group>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row className="align-items-center" gutter={{ xs: 0, sm: 0, lg: 0 }}>
+            <Col xs={24} sm={24} md={11} lg={11}>
+              <Form.Item
+                name="ageyearsmonths"
+                label="Age In Years & Months"
+                rules={rules.ageyearsmonths}
+              >
+                <div className="justify-content-between d-flex">
+                  <Input
+                    className="w-48"
+                    type="number"
+                    placeholder="Years"
+                    value={ageYearsMonths?.years}
+                    onChange={(e) => {
+                      setAgeYearsMonths({
+                        ...ageYearsMonths,
+                        years: e.target.value,
+                      });
+                    }}
+                  />
+                  <Input
+                    className="w-48"
+                    type="number"
+                    placeholder="Months"
+                    maxLength={2}
+                    value={ageYearsMonths?.months}
+                    onChange={(e) => {
+                      setAgeYearsMonths({
+                        ...ageYearsMonths,
+                        months: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={2} lg={2}>
+              <div className="or text-center mt-2">OR</div>
+            </Col>
+            <Col xs={24} sm={24} md={11} lg={11}>
+              <Form.Item name="dateofbirth" label="Date of Birth">
+                <DatePicker
+                  onChange={onBirthDateChanged}
+                  value={
+                    birthDate
+                      ? dayjs(getFormattedDate(birthDate), "YYYY-MM-DD")
+                      : null
+                  }
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      )}
     </>
   );
 }
