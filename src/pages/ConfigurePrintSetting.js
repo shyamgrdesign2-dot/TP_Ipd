f548add434558f4bd78315cc7e3646ad22dc91c0
Ajeet Singh <ajeet.singh@strausshealthcare.com>
2024-02-23 17:58:57 +0530

    Configure Print Setting

--- src/pages/ConfigurePrintSetting.js
+++ src/pages/ConfigurePrintSetting.js
@@ -48,15 +48,15 @@ function ConfigurePrintSetting() {
             <div className={'w-100 bg-body wrapper2'}>
                 <Flex justify="space-between">
                     <Col flex={1} className="me-4">
-                        <div className="bg-white h-100">
+                        <div className="bg-white overflow-y-auto" style={{height: 'calc(100vh - 60px)'}}>
                             <Tabs defaultActiveKey="1" items={TabsPrintSetting} onChange={onChange} />
                         </div>
                     </Col>
-                    <Col flex={8}>
-                        <div className="mx-auto" style={{width: 1240}}>
+                    <Col flex={8} className="overflow-y-auto" style={{height: 'calc(100vh - 60px)'}}>
+                        <div className="mx-auto overflow-y-auto" style={{width: 1240}}>
                             <div className="titleprint mt-20" onClick={() => printContent()}>Preview</div>
                             <div className="border rounded-20px bg-white mt-20 overflow-hidden h-100">
-                                <div key={Math.random()} ref={printRef} style={{padding: 20}} className="h-100 overflow-y-auto">
+                                <div key={Math.random()} ref={printRef} style={{padding: 20}} className="h-100">
                                     <PrintHtmlPage />
                                 </div>
                             </div>
