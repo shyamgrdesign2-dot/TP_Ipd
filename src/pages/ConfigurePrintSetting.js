45e56a515edd0a80695ef580cd0e12646e98f8b3
Ajeet Singh <147466456+Ajeetstrauss@users.noreply.github.com>
2024-03-01 17:32:36 +0530

    print setting

--- src/pages/ConfigurePrintSetting.js
+++ src/pages/ConfigurePrintSetting.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Flex, Tabs, Radio, Row, Form, Select } from "antd";
+import { Col, Tabs, Radio, Row, Form, Select, Switch, Button, Input } from "antd";
 import HeaderPrintSetting from "../common/HeaderPrintSetting";
 import { useReactToPrint } from 'react-to-print';
 import PrintHtmlPage from "./PrintHtmlPage";
@@ -14,8 +14,8 @@ function ConfigurePrintSetting() {
             key: '1',
             label: 'Prescription',
             children:
-                <div className="px-2">
-                    <div className="title-common mb-3">Format Style</div>
+                <div className="px-3">
+                    <div className="titleprint mb-3">Format Style</div>
                     <Row justify="space-between" className="align-items-center form_addnewpatient mb-3">
                         <Col lg="10">
                             All Change to
@@ -169,6 +169,7 @@ function ConfigurePrintSetting() {
                     <Row justify="space-between" className="align-items-center form_addnewpatient mb-3">
                         <Col lg="10">
                             <div className="d-flex align-items-center">
+                                {/* disable-preview  Add and remove this class for desable enable */}
                                 <i className="icon-Preview disable-preview me-2"></i>
                                 <span>Vaccination</span>
                             </div>
@@ -188,13 +189,190 @@ function ConfigurePrintSetting() {
         {
             key: '2',
             label: 'Header & Footer',
-            children: 'Content of Tab Pane 2',
+            children: <div className="px-4 form_addnewpatient">
+                <div className="border-bottom pb-3 mb-3">
+                    <Row justify="space-between" className="align-items-center form_addnewpatient mb-1">
+                        <Col lg="18">
+                            <div className="titleprint">Upload Header & Footer</div>
+                        </Col>
+                        <Col lg="6">
+                            <Switch />
+                        </Col>
+                    </Row>
+                    <div>You can upload your own custom header & footer image</div>
+
+                    <div className="upload-headfoot">
+                        <div className="fw-medium text-decoration-underline cursor-pointer">Upload Header</div>
+                        <div className="fs-12-1 fontroboto"> Only jpg, jpeg or png files with the max size 2mb.</div>
+                        <Button className="btn btn-headfoot"><i className="icon-Edit me-1"></i>Edit</Button>
+                    </div>
+                    <div className="upload-headfoot">
+                        <div className="fw-medium text-decoration-underline cursor-pointer">Upload Footer</div>
+                        <div className="fs-12-1 fontroboto"> Only jpg, jpeg or png files with the max size 2mb.</div>
+                        <Button className="btn btn-headfoot"><i className="icon-Edit me-1"></i>Edit</Button>
+                    </div>
+                    <Form.Item className="mb-0">
+                        <label className="mb-1">Letterhead Used In</label>
+                        <Radio.Group className="d-flex gender-radio">
+                            <Radio.Button className="w-100 text-center" value="Male">Both</Radio.Button>
+                            <Radio.Button className="w-100 text-center" value="Female">Print Rx</Radio.Button>
+                            <Radio.Button className="w-100 text-center" value="Other">WhatsApp Rx</Radio.Button>
+                        </Radio.Group>
+                    </Form.Item>
+
+                </div>
+                <div className="border-bottom pb-3 mb-3">
+                    <Row justify="space-between" className="align-items-center form_addnewpatient mb-1">
+                        <Col lg="18">
+                            <div className="titleprint">Print Rx on your letter head</div>
+                        </Col>
+                        <Col lg="6">
+                            <Switch />
+                        </Col>
+                    </Row>
+                    <div>Use page margins to display your letterhead safely on print</div>
+
+                    <div className="mt-3">
+                        <div className="d-flex mt-3">
+                            <div className="me-4">
+                                <label className="mb-1 title-common">Top Margin</label>
+                                <div className="d-flex align-items-center justify-content-between mb-12">
+                                    <Input className='inputheight41-group' inputMode="numeric" addonAfter={'cm'} />
+                                </div>
+                            </div>
+                            <div>
+                                <label className="mb-1 title-common">Bottom Margin</label>
+                                <div className="d-flex align-items-center justify-content-between mb-12">
+                                    <Input className='inputheight41-group' inputMode="numeric" addonAfter={'cm'} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="d-flex mt-3">
+                            <div className="me-4">
+                                <label className="mb-1 title-common">Left Margin</label>
+                                <div className="d-flex align-items-center justify-content-between mb-12">
+                                    <Input className='inputheight41-group' inputMode="numeric" addonAfter={'cm'} />
+                                </div>
+                            </div>
+                            <div>
+                                <label className="mb-1 title-common">Right Margin</label>
+                                <div className="d-flex align-items-center justify-content-between mb-12">
+                                    <Input className='inputheight41-group' inputMode="numeric" addonAfter={'cm'} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="border-bottom pb-3 mb-3">
+                    <Row justify="space-between" className="align-items-center form_addnewpatient mb-1">
+                        <Col lg="18">
+                            <div className="titleprint">Custom Header</div>
+                        </Col>
+                        <Col lg="6">
+                            <Button className="btn rounded-10px px-1 border" style={{ transform: "rotate(-89deg)" }}>
+                                <i className="icon-right"></i>
+                            </Button>
+                        </Col>
+                    </Row>
+                    <div>Create your custom header</div>
+                    <div className="mt-3">
+                        <Form.Item>
+                            <label className="mb-1">Header Title</label>
+                            <Input className='inputheight41-group' inputMode="numeric" />
+                        </Form.Item>
+                    </div>
+                    <div className="mt-3">
+                        <Form.Item>
+                            <label className="mb-1">Header Font Size</label>
+                            <Select
+                                showSearch
+                                className="autocomplete-custom"
+                                allowClear
+                            />
+                        </Form.Item>
+                    </div>
+                    <div className="mt-3">
+                        <Form.Item>
+                            <label className="mb-1">Header Title</label>
+                            <Input className='inputheight41-group' inputMode="numeric" />
+                        </Form.Item>
+                    </div>
+                    <div className="mt-3">
+                        <Form.Item>
+                            <label className="mb-1">Header Font Size</label>
+                            <Select
+                                showSearch
+                                className="autocomplete-custom"
+                                allowClear
+                            />
+                        </Form.Item>
+                    </div>
+                    <Row justify="space-between" className="align-items-center form_addnewpatient mb-1">
+                        <Col lg="18">
+                            <div className="title-common">Logo on Header</div>
+                        </Col>
+                        <Col lg="6">
+                            <Switch />
+                        </Col>
+                    </Row>
+                    <div className="upload-headfoot upload-headfoot1 p-3">
+                        <div className="d-flex align-items-center justify-content-between">
+                            <div className="text-start fontroboto">Upload a picture of your<br /> Logo</div>
+                            <div className="btn btn-input btn-41 d-flex align-items-center justify-content-center">
+                                <Form.Item name="pm_image" />
+                                <input type="file" accept="image/*" />
+                                 <span>Upload</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="border-bottom pb-3 mb-3">
+                    <Row justify="space-between" className="align-items-center form_addnewpatient mb-1">
+                        <Col lg="18">
+                            <div className="titleprint">Display Patient Info</div>
+                        </Col>
+                        <Col lg="6">
+                            <Button className="btn rounded-10px px-1 border" style={{ transform: "rotate(-89deg)" }}>
+                                <i className="icon-right"></i>
+                            </Button>
+                        </Col>
+                    </Row>
+                    <div>Manage your patient information</div>
+                </div>
+                <div className="border-bottom pb-3 mb-3">
+                    <Row justify="space-between" className="align-items-center form_addnewpatient mb-1">
+                        <Col lg="18">
+                            <div className="titleprint">Custom Footer</div>
+                        </Col>
+                        <Col lg="6">
+                            <Button className="btn rounded-10px px-1 border" style={{ transform: "rotate(-89deg)" }}>
+                                <i className="icon-right"></i>
+                            </Button>
+                        </Col>
+                    </Row>
+                    <div>Create your custom footer</div>
+                </div>
+                <div className="mb-3">
+                    <Row justify="space-between" className="align-items-center form_addnewpatient mb-1">
+                        <Col lg="18">
+                            <div className="titleprint">Other Settings</div>
+                        </Col>
+                        <Col lg="6">
+                            <Button className="btn rounded-10px px-1 border" style={{ transform: "rotate(-89deg)" }}>
+                                <i className="icon-right"></i>
+                            </Button>
+                        </Col>
+                    </Row>
+                    <div>Customize your watermark, signature, and QR code</div>
+                </div>
+            </div>,
         },
         {
             key: '3',
             label: 'Page Format',
-            children: <div className="px-2 form_addnewpatient">
-                <div className="title-common mb-3">Page Layout</div>
+            children: <div className="px-4 form_addnewpatient">
+                <div className="titleprint mb-3">Page Layout</div>
                 <Form.Item>
                     <label className="mb-1">Page Size</label>
                     <Radio.Group className="d-flex gender-radio">
@@ -254,7 +432,7 @@ function ConfigurePrintSetting() {
                 <Row justify="space-between">
                     <Col lg={8} className="pe-4">
                         <div className="bg-white overflow-y-auto" style={{ height: 'calc(100vh - 60px)' }}>
-                            <Tabs defaultActiveKey="1" items={TabsPrintSetting} onChange={onChange} />
+                            <Tabs defaultActiveKey="1" items={TabsPrintSetting} onChange={onChange} className="print-tabs" />
                         </div>
                     </Col>
                     <Col lg={16} className="overflow-y-auto" style={{ height: 'calc(100vh - 60px)' }}>
