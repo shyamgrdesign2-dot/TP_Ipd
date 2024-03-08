262baf86ff3c62bf19c2f185890a40c1850f1b87
Kishan <kishan.patel@strausshealthcare.com>
2024-03-08 14:54:14 +0530

    up to date

--- src/components/MedicationsBox.js
+++ src/components/MedicationsBox.js
@@ -123,8 +123,8 @@ function MedicationsBox() {
     [searchParentQuery]
   );
 
-  const onSelectParent = async (data, e) => {
-    const action = await dispatch(getMedicineDetails(JSON.parse(e.key).tmm_id));
+  const onSelectParent = async (data, item) => {
+    const action = await dispatch(getMedicineDetails(JSON.parse(item.key).tmm_id));
     if (action.meta.requestStatus === "fulfilled") {
       const updatedData = action.payload.map((e) => {
 
@@ -134,6 +134,7 @@ function MedicationsBox() {
 
         return {
           ...e,
+          objectID: JSON.parse(item.key).objectID,
           tmm_unit_name: unitObj && unitObj !== undefined ? unitObj.tmu_title : "",
           tmm_freq_type_name: e.tmf_block == 0 ?
             `${e.tcm_tmm_freq_morning ? e.tcm_tmm_freq_morning + " - " : "0 -"}${e.tcm_tmm_freq_afternoon ? e.tcm_tmm_freq_afternoon + " - " : "0 -"}${e.tcm_tmm_freq_evening ? e.tcm_tmm_freq_evening + " - " : "0 -"}${e.tcm_tmm_freq_night ? e.tcm_tmm_freq_night : "0"}`
@@ -617,7 +618,6 @@ function MedicationsBox() {
         title={"You may lose your data"}
         modalBody={
           <>
-            {console.log("first")}
             <div className="alert-warning rounded-10px p-2 patient-details">
               <div className="d-flex align-items-center">
                 <img className='me-3' src={alertIcon} alt="Warning" />
