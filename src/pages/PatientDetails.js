b16dca4b5dd7614889a1cc3b0fd868f4301e2476
Ajeet Singh <ajeet.singh@strausshealthcare.com>
2024-06-26 10:33:52 +0530

    no message

--- src/pages/PatientDetails.js
+++ src/pages/PatientDetails.js
@@ -118,7 +118,7 @@ function PatientDetails() {
                                         {viewCaseManagerData && viewCaseManagerData?.vitals?.length > 0 && (
                                             <VitalsBodyComposition loading={loading} passVitals={viewCaseManagerData ? [...viewCaseManagerData.vitals].slice(0, 2) : viewCaseManagerData} />
                                         )}
-                                        {!viewCaseManagerData?.smart_prescription_filename &&
+                                        {viewCaseManagerData?.smart_prescription_filename === null &&
                                             <>
                                                 <MedicalHistory loading={loading} medicalHistoryData={viewCaseManagerData?.medical_history} />
                                                 <VisitVaccination />
