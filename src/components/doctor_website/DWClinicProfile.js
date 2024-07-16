0d5a4e5aae8423e5843b0788ecd27726918a8b53
Ajeet Singh <ajeet.singh@strausshealthcare.com>
2024-07-16 17:40:16 +0530

    Changes

--- src/components/doctor_website/DWClinicProfile.js
+++ src/components/doctor_website/DWClinicProfile.js
@@ -1,18 +1,17 @@
 import React, { useState, useCallback } from 'react';
-import { Button, Collapse, Form, Input, Tabs, Row, Col, Select } from 'antd';
+import { Button, Collapse, Form, Input, Tabs, Row, Col, TimePicker } from 'antd';
 
 import { Address, Timings, Photos } from "../../utils/constants";
 
 function DWClinicProfile() {
-
-    const specialtyList = [
-        {
-            value: 'anaesthesiology',
-            label: 'Anaesthesiology',
-        }
-    ]
-
     const [selectedTab, setSelectedTab] = useState();
+
+    const onTabChange = useCallback(
+        (key) => {
+            setSelectedTab(key);
+        },
+        [selectedTab]
+    );
 
     const TabsPrintSetting = [
         {
@@ -29,14 +28,73 @@ function DWClinicProfile() {
         },
     ];
 
-    const onTabChange = useCallback(
-        (key) => {
-            setSelectedTab(key);
+    const TimingItems = [
+        {
+            key: '1',
+            label:
+                <>
+                    <div className="title-common">Shift 1</div>
+                    <div className='fontroboto'>Manage your shifts with new set of days</div>
+                </>,
+            children:
+                <>
+                    <div className='p-10 pt-3'>
+                        <Form.Item
+                            label="Select multiple days for create the shift"
+                            className='fw-medium mb-1'
+                            required>
+                                <div className='d-flex align-items-center justify-content-between'>
+                                    <div className='days-width sunday-color rounded-10px border'>S</div> 
+                                    <div className='days-width active-days rounded-10px border'>M</div> 
+                                    <div className='days-width active-days rounded-10px border'>T</div> 
+                                    <div className='days-width active-days rounded-10px border'>W</div> 
+                                    <div className='days-width active-days rounded-10px border'>T</div> 
+                                    <div className='days-width rounded-10px border'>F</div> 
+                                    <div className='days-width rounded-10px border'>S</div> 
+                                </div>
+                        </Form.Item>
+                    </div>
+                    <Form.Item
+                        label="Set your shift timings"
+                        className='fw-medium p-10 pb-0 mb-0'
+                        required>
+                        <Row align="middle" justify="space-between" className='mb-12'>
+                            <Col span={9}>
+                                <TimePicker format="h:mm A" use12Hours placeholder="hh:mm" className="text-capitalize rounded-10px h-38" />
+                            </Col>
+                            <Col span={2}>
+                                <div className='text-center'>To</div>
+                            </Col>
+                            <Col span={9}>
+                                <TimePicker format="h:mm A" use12Hours placeholder="hh:mm" className="text-capitalize rounded-10px h-38" />
+                            </Col>
+                            <Col span='auto'>
+                                <Button className='btn-delete-prescription btn px-0'><i className='icon-Add text-primary'></i></Button>
+                            </Col>
+                        </Row>
+                        <Row align="middle" justify="space-between" className='mb-12'>
+                            <Col span={9}>
+                                <TimePicker format="h:mm A" use12Hours placeholder="hh:mm" className="text-capitalize rounded-10px h-38" />
+                            </Col>
+                            <Col span={2}>
+                                <div className='text-center'>To</div>
+                            </Col>
+                            <Col span={9}>
+                                <TimePicker format="h:mm A" use12Hours placeholder="hh:mm" className="text-capitalize rounded-10px h-38" />
+                            </Col>
+                            <Col span='auto'>
+                                <Button className='btn-delete-prescription btn px-0'><i className='icon-delete text-primary'></i></Button>
+                            </Col>
+                        </Row>
+                        <button className='btn float-end mb-2 btn-text d-flex px-0'><i className='icon-delete fs-18 me-1'></i><span>Delete Shift</span></button>
+                    </Form.Item>
+                </>
         },
-        [selectedTab]
-    );
+    ];
 
-    const accordionItems = [
+    const inputWatermarkFile = React.createRef();
+
+    const clinicItems = [
         {
             key: '1',
             label:
@@ -64,7 +122,7 @@ function DWClinicProfile() {
                             </div>
                             <Tabs defaultActiveKey="1" onChange={onTabChange} items={TabsPrintSetting} className="print-tabs" />
                             {selectedTab === Address ? (
-                                <Row gutter={20} className='px-20'>
+                                <Row gutter={20} className='px-10'>
                                     <Col span={12}>
                                         <Form.Item
                                             label="Pincode"
@@ -105,54 +163,23 @@ function DWClinicProfile() {
                                     </Col>
                                 </Row>
                             ) : selectedTab === Timings ? (
-                                <Row gutter={20} className='px-20'>
-                                    <Col span={12}>
-                                        <Form.Item
-                                            label="Pincode"
-                                            className='fw-medium mb-20'
-                                            required>
-                                            <Input placeholder="Pincode" className="text-capitalize rounded-10px h-38" />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col span={12}>
-                                        <Form.Item
-                                            label="City"
-                                            className='fw-medium mb-20'
-                                            required>
-                                            <Input placeholder="City" className="text-capitalize rounded-10px h-38" />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col span={24}>
-                                        <Form.Item
-                                            label="State"
-                                            className='fw-medium mb-20'
-                                            required>
-                                            <Input placeholder="City" className="text-capitalize rounded-10px h-38" />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col span={24}>
-                                        <Form.Item
-                                            label="Address"
-                                            className='fw-medium mb-20'>
-                                            <Input.TextArea placeholder="Address" rows={3} className="textareaPlaceholder text-capitalize rounded-10px" />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col span={24}>
-                                        <Form.Item
-                                            label="Google map link"
-                                            className='fw-medium mb-20'>
-                                            <Input placeholder="Copy and paste your clinic address link" className="text-capitalize rounded-10px h-38" />
-                                        </Form.Item>
-                                    </Col>
-                                </Row>
+                                <div className='px-10 mb-2'>
+                                    <Collapse items={TimingItems} defaultActiveKey={['1']} className='prescriptiontab-accordian timingTab' expandIconPosition={'end'} />
+                                    <button className='mt-2 btn btn-delete-experience text-primary'><i className='icon-Add fs-18 me-2'></i>Add More Shifts</button>
+                                </div>
                             ) : selectedTab === Photos && (
-                                '655656564'
+                                <div className='px-10'>
+                                    <div className="upload-headfoot mt-0" onClick={() => inputWatermarkFile.current?.click()}>
+                                        <input ref={inputWatermarkFile} className="image-upload-input" style={{ display: 'none' }} type="file" accept="image/png" />
+                                        <div className="fw-medium text-decoration-underline cursor-pointer">Upload Clinic Photos</div>
+                                        <div className="fs-12-1 fontroboto">  Only jpg, jpeg or png files with the max size 5mb.</div>
+                                    </div>
+                                </div>
                             )}
                         </Form>
                     </div>
-                    <Button className='btn w-100 btn-delete-experience btn-41 rounded-top-0 btn-primary3' disabled><i className='icon-delete fs-18 me-2'></i>Delete</Button>
+                    {/* <Button className='btn w-100 btn-delete-experience btn-41 rounded-top-0 btn-primary3' disabled><i className='icon-delete fs-18 me-2'></i>Delete</Button> */}
                 </div>,
-
         },
     ];
 
@@ -161,9 +188,9 @@ function DWClinicProfile() {
             <div className='p-20'>
                 <div className="text-greycolor fontroboto"> Add your clinic name, location, timings, and photos.</div>
                 <div className="border rounded-20px bg-white mt-3">
-                    <Collapse items={accordionItems} defaultActiveKey={['1']} className="prescriptiontab-accordian doctor-experience" expandIconPosition={'end'} />
+                    <Collapse items={clinicItems} defaultActiveKey={['1']} className="prescriptiontab-accordian doctor-experience" expandIconPosition={'end'} />
                 </div>
-                <Button className='btn btn-input w-100 btn-41 d-flex align-items-center justify-content-center mt-3'><i className='icon-Add fs-18 me-2'></i>Add Education</Button>
+                <Button className='btn btn-input w-100 btn-41 d-flex align-items-center justify-content-center mt-3'><i className='icon-Add fs-18 me-2'></i>Add one more clinic</Button>
             </div>
         </div>
     );
