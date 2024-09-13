376c9c1b13f9214e1f410f2b61010fa58e8c534e
Ajeet Singh <ajeet.singh@strausshealthcare.com>
2024-09-13 15:58:27 +0530

    Added Clear button in web

--- src/components/DiagnosisBox.js
+++ src/components/DiagnosisBox.js
@@ -59,6 +59,7 @@ function DiagnosisBox() {
   const [allTemplates, setAllTemplates] = useState([]);
   const [matchedTemplates, setMatchedTemplates] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen1, setIsModalOpen1] = useState(false);
   const [removeTemplateId, setRemoveTemplateId] = useState(null);
 
   const [searchParentQuery, setSearchParentQuery] = useState("");
@@ -711,6 +712,14 @@ function DiagnosisBox() {
     );
   }, [tabChange, popOver2, inputTemplateName, loading, allTemplates]);
 
+  function onRemoveSymptoms() {
+    setDiagnosisData([])
+  }
+
+  const showHideModal1 = useCallback(() => {
+    setIsModalOpen1(!isModalOpen1);
+  }, [isModalOpen1]);
+
   return (
     <>
       <div className="">
@@ -750,6 +759,38 @@ function DiagnosisBox() {
                 </button>
               </Popover>
             </Tooltip>
+            <button onClick={showHideModal1} className="btn btn-text px-1">
+              <i className="icon-eraser1"></i>
+            </button>
+            <CommonModal
+              isModalOpen={isModalOpen1}
+              onCancel={showHideModal1}
+              modalWidth={500}
+              title={"You may lose your data"}
+              modalBody={
+                <>
+                  <div className="alert-warning rounded-10px p-2 patient-details">
+                    <div className="d-flex align-items-center">
+                      <img className='me-3' src={alertIcon} alt="Warning" />
+                      <span>
+                        Are you sure you want to Clear Selected <b>Symptoms</b>?
+                      </span>
+                    </div>
+                  </div>
+                  <div className="mt-4">
+                    <div className="d-flex align-items-center mt-2 justify-content-end">
+                      <div onClick={() => onRemoveSymptoms()}
+                        className="me-4 text-decoration-underline btn p-0 text-main">
+                        <span onClick={showHideModal1}>Yes, Clear</span>
+                      </div>
+                      <Button onClick={showHideModal1} className="lh-lg btn btn-primary3 btn-41 px-4">
+                        <span>No</span>
+                      </Button>
+                    </div>
+                  </div>
+                </>
+              }
+            />
           </div>
         </div>
 
