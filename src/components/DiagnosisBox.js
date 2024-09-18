ff3b16fed4f238a2180857f5b62e5bfe6b6d90cd
Ajeet Singh <ajeet.singh@strausshealthcare.com>
2024-09-18 15:17:34 +0530

    Clear button changes

--- src/components/DiagnosisBox.js
+++ src/components/DiagnosisBox.js
@@ -795,8 +795,8 @@ function DiagnosisBox() {
                 </button>
               </Popover>
             </Tooltip>
-            <button onClick={showHideClearData} className="btn btn-text px-1">
-              <i className="icon-eraser1"></i>
+            <button onClick={showHideClearData} className="btn btn-text clear-text d-flex align-items-center" disabled={diagnosisData.length > 0 ? false : true}>
+              <i className="icon-eraser1 me-2"></i> <span>Clear</span>
             </button>
           </div>
         </div>
