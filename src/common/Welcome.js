9562e7d6cc9190da82dedd6ce08f2bcac4b69824
Akhil Goudar <akhilgoudar@Akhils-Laptop.local>
2024-09-27 16:00:59 +0530

    OPD plans changes.

--- src/common/Welcome.js
+++ src/common/Welcome.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect,useState,useRef,useCallback } from "react";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -7,6 +7,10 @@ import config from "../config";
 import { env } from "../EnvironmentConfig";
 import { OPD_API_KEY } from "../utils/constants";
 import axios from "axios";
+import { QRCodeSVG } from 'qrcode.react';
+import { Button as ButtonOPD } from "antd";
+import CommonModal from "./CommonModal";
+import { useFeatureIsOn } from "@growthbook/growthbook-react";
 
 function Welcome(props) {
 
@@ -17,6 +21,30 @@ function Welcome(props) {
   const { profile } = useSelector((state) => state.doctors);
   const decodedToken = getDecodedToken();
   const apiUrl = env.opd_encryption_url;
+  const [isQRCodeVisible, setQRCodeVisible] = useState(false);
+  const [opdPlansUrl, setOpdPlansUrl] = useState(null);
+  const [isBackModalOpen, setIsBackModalOpen] = useState(false);
+  const printRef = useRef();
+
+  const isOpdPlansAccessableFromGB = useFeatureIsOn(
+    "opd-plans"
+  );
+
+  // Handle opening the QR code modal
+  const handleShowQRCode = () => {
+    setQRCodeVisible(true);
+  };
+
+  const showHideBackModal = useCallback(() => {
+    setQRCodeVisible(false);
+  }, [isQRCodeVisible]);
+
+  useEffect(() => {
+    if (isQRCodeVisible && !opdPlansUrl) {
+      clickOpdPlans();  // Trigger the API call when QR modal is visible and URL isn't yet set
+    }
+  }, [isQRCodeVisible]);
+
 
   const clickWalkInConsultation = () => {
     const businessId = decodedToken?.result?.hospital_business_id;
@@ -46,21 +74,50 @@ function Welcome(props) {
     }
   };
 
-  const clickOpdPlans = async() => {
+  const clickOpdPlans = async () => {
     const clinic_Id = decodedToken?.result?.clinic_id;
     const doc_Id = decodedToken?.result?.doctor_unique_id;
-    const clinic_Data = {
-      c_id:clinic_Id
-    }
-    const doc_Data = {
-      d_id:doc_Id
-    }
-  
-    const c_id = await opdEncryptionApiCall(clinic_Data)
-    const d_id = await opdEncryptionApiCall(doc_Data)
-    const opdPlansUrl = `https://visit-enrolment-tatva.getvisitapp.net/tatva-care?d_id=${d_id}&c_id=${c_id}`
-    console.log(opdPlansUrl,"opdPlansUrl")
-  }
+    
+    const clinic_Data = { c_id: clinic_Id };
+    const doc_Data = { d_id: doc_Id };
+
+    // Encrypt clinic and doctor ID
+    const c_id = await opdEncryptionApiCall(clinic_Data);
+    const d_id = await opdEncryptionApiCall(doc_Data);
+    
+    // Generate and set the URL
+    const url = `https://visit-enrolment-tatva.getvisitapp.net/tatva-care?d_id=${d_id}&c_id=${c_id}`
+    // console.log(url,"url")
+    setOpdPlansUrl(url);  // Set the URL after the API call
+  };
+
+  // Handle print functionality
+  const handlePrint = () => {
+    const printContent = printRef.current.innerHTML;
+    const win = window.open('', '', 'height=500,width=500');
+    win.document.write(`
+      <html>
+        <head>
+          <title>Print QR Code</title>
+          <style>
+            @media print {
+              button {
+                display: none;
+              }
+            }
+          </style>
+        </head>
+        <body>
+          ${printContent}
+        </body>
+      </html>
+    `);
+    win.document.close();
+    win.focus();
+    win.print();
+    win.close();
+  };
+
 
   return (
     <>
@@ -92,13 +149,14 @@ function Welcome(props) {
           </div>
           <div className="d-flex gap-1">
             <div>
-              {locationPath == "/" && (
+              {locationPath == "/" && isOpdPlansAccessableFromGB && (
                 <div className="d-lg-flex d-block">
                   {/* <Button variant="outline-primary me-3 d-flex align-items-center mb-lg-0 mb-2" onClick={() => alert('Comming Soon')}> <i className={'icon-Add me-2'}></i> {'Add New Appointment'}</Button> */}
                   <Button
                     variant="primary"
                     className="px-3 btn-41"
-                    onClick={clickOpdPlans}
+                    // onClick={clickOpdPlans}
+                    onClick={handleShowQRCode}
                   >
                     {"OPD Plans"}
                   </Button>
@@ -119,6 +177,31 @@ function Welcome(props) {
               )}
             </div>
           </div>
+          <CommonModal
+                  isModalOpen={(isQRCodeVisible && opdPlansUrl)}
+                  onCancel={showHideBackModal}
+                  modalWidth={500}
+                  title={"QR Code for OPD plans"}
+                  modalBody={
+                    <>
+                      <div className="alert-warning rounded-10px p-2 patient-details">
+                        <div ref={printRef} className="d-flex align-items-center justify-content-center">
+                          <QRCodeSVG  value={opdPlansUrl} size={256} />
+                        </div>
+                      </div>
+                      <div className="mt-4">
+                        <div className="d-flex align-items-center mt-2 justify-content-center">
+                          <ButtonOPD
+                            onClick={handlePrint}
+                            className="lh-lg btn btn-primary3 btn-41 px-4"
+                          >
+                            <span>Print</span>
+                          </ButtonOPD>
+                        </div>
+                      </div>
+                    </>
+                  }
+                />
         </div>
         <div className="pb-5">&nbsp;</div>
       </div>
