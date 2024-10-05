7489fb505babb6f3b33b1f0de10ce741596d65e9
harish-tatvacare <harish.y@tatvacare.in>
2024-10-05 23:07:35 +0530

    image preview changes

--- src/pages/medicalRecords/components/documentPreview/DocumentPreview.js
+++ src/pages/medicalRecords/components/documentPreview/DocumentPreview.js
@@ -2,11 +2,16 @@ import { Button, Card } from "antd";
 import { Worker, Viewer, RotateDirection } from "@react-pdf-viewer/core";
 import { zoomPlugin } from "@react-pdf-viewer/zoom";
 import "./DocumentPreview.scss";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { pageNavigationPlugin } from "@react-pdf-viewer/page-navigation";
 import { rotatePlugin } from "@react-pdf-viewer/rotate";
 import clockwise from "./../../../../assets/images/clockwise-direction.svg";
 import anticlockwise from "./../../../../assets/images/anticlockwise-direction.svg";
+import {
+  TransformWrapper,
+  TransformComponent,
+  useControls,
+} from "react-zoom-pan-pinch";
 import { useSelector } from "react-redux";
 import { isChrome, isSafari } from "react-device-detect";
 
@@ -31,6 +36,26 @@ const DocumentPreview = ({
   const { Rotate } = rotatePluginInstance;
   const pageNavigationPluginInstance = pageNavigationPlugin();
   const { ZoomIn, ZoomOut } = zoomPluginInstance;
+  const [isPdf, setIsPdf] = useState(true);
+  const imgRef = useRef(null);
+  const [angle, setAngle] = useState(0);
+
+  useEffect(() => {
+    if (cardData?.url?.includes(".pdf")) {
+      setIsPdf(true);
+    } else {
+      setIsPdf(false);
+      setNumberOfPages(1);
+    }
+  }, [cardData]);
+
+  const rotateLeft = () => {
+    setAngle((prevAngle) => prevAngle - 90);
+  };
+
+  const rotateRight = () => {
+    setAngle((prevAngle) => prevAngle + 90);
+  };
 
   const onLoadSuccess = (pdfDocument) => {
     setNumberOfPages(pdfDocument?.doc?._pdfInfo?.numPages || 0);
@@ -42,6 +67,121 @@ const DocumentPreview = ({
 
   const handleOnZoom = (zoomCount) => {
     setScale(zoomCount?.scale);
+  };
+
+  const Controls = ({ handleImgZoomOut, handleImgZoomIn }) => {
+    return (
+      <div
+        className="controls mb-4 d-flex align-items-center"
+        style={{
+          background: "rgba(0, 0, 0, 0.8)",
+          padding: 12,
+          borderRadius: 12,
+          columnGap: 30,
+          position: "absolute",
+          bottom: 30,
+          zIndex: 1,
+        }}
+      >
+        <div
+          className="document-measurement"
+          style={{ display: "flex", alignItems: "center", columnGap: "8px" }}
+        >
+          <div
+            className="document-measurement-box document-measurement"
+            style={{ width: 38, textAlign: "center" }}
+          >
+            {currentPage}
+          </div>{" "}
+          / {numberOfPages}
+        </div>
+        <div>
+          <span className="document-separator" />
+        </div>
+        <div
+          style={{ display: "flex", alignItems: "center", columnGap: "8px" }}
+        >
+          <ZoomOut>
+            {(props) => (
+              <Button
+                type="text"
+                className="btn btn-delete-prescription px-0 focus-none h-100"
+                onClick={isPdf ? props.onClick : handleImgZoomOut}
+              >
+                <i className="icon-minus" style={{ color: "white" }} />
+              </Button>
+            )}
+          </ZoomOut>
+          <span
+            className="mx-2 document-measurement document-measurement-box"
+            style={{ minWidth: 70, textAlign: "center" }}
+          >
+            {Math.round(scale * 100)}%
+          </span>
+          <ZoomIn>
+            {(props) => (
+              <Button
+                type="text"
+                className="btn btn-delete-prescription px-0 focus-none h-100"
+                onClick={isPdf ? props.onClick : handleImgZoomIn}
+              >
+                <i className="icon-Add" style={{ color: "white" }} />
+              </Button>
+            )}
+          </ZoomIn>
+        </div>
+        <div>
+          <span className="document-separator" />
+        </div>
+        <div
+          style={{ display: "flex", alignItems: "center", columnGap: "8px" }}
+        >
+          <Rotate direction={RotateDirection.Backward}>
+            {(props) => (
+              <Button
+                type="text"
+                className="btn btn-delete-prescription px-0 focus-none h-100"
+                onClick={isPdf ? props.onClick : rotateLeft}
+              >
+                <img src={anticlockwise} alt="anticlockwise-direction" />
+              </Button>
+            )}
+          </Rotate>
+          <Rotate direction={RotateDirection.Forward}>
+            {(props) => (
+              <Button
+                type="text"
+                className="btn btn-delete-prescription px-0 focus-none h-100"
+                onClick={isPdf ? props.onClick : rotateRight}
+              >
+                <img src={clockwise} alt="clockwise-direction" />
+              </Button>
+            )}
+          </Rotate>
+        </div>
+      </div>
+    );
+  };
+
+  const ImageControls = () => {
+    const { zoomIn, zoomOut, state } = useControls();
+
+    const handleImgZoomIn = (state) => {
+      zoomIn();
+      setScale(state?.step || 1);
+    };
+
+    const handleImgZoomOut = (state) => {
+      zoomOut();
+      setScale(state?.step || 1);
+    };
+
+    return (
+      <Controls
+        handleImgZoomOut={handleImgZoomOut}
+        handleImgZoomIn={handleImgZoomIn}
+      />
+    );
   };
 
   return (
@@ -129,95 +269,7 @@ const DocumentPreview = ({
       </Card>
 
       <div className="d-flex flex-column justify-content-center align-items-center">
-        <div
-          className="controls mb-4 d-flex align-items-center"
-          style={{
-            background: "rgba(0, 0, 0, 0.8)",
-            padding: 12,
-            borderRadius: 12,
-            columnGap: 30,
-            position: "absolute",
-            bottom: 30,
-            zIndex: 1,
-          }}
-        >
-          <div
-            className="document-measurement"
-            style={{ display: "flex", alignItems: "center", columnGap: "8px" }}
-          >
-            <div
-              className="document-measurement-box document-measurement"
-              style={{ width: 38, textAlign: "center" }}
-            >
-              {currentPage}
-            </div>{" "}
-            / {numberOfPages}
-          </div>
-          <div>
-            <span className="document-separator" />
-          </div>
-          <div
-            style={{ display: "flex", alignItems: "center", columnGap: "8px" }}
-          >
-            <ZoomOut>
-              {(props) => (
-                <Button
-                  type="text"
-                  className="btn btn-delete-prescription px-0 focus-none h-100"
-                  onClick={props.onClick}
-                >
-                  <i className="icon-minus" style={{ color: "white" }} />
-                </Button>
-              )}
-            </ZoomOut>
-            <span
-              className="mx-2 document-measurement document-measurement-box"
-              style={{ minWidth: 70, textAlign: "center" }}
-            >
-              {Math.round(scale * 100)}%
-            </span>
-            <ZoomIn>
-              {(props) => (
-                <Button
-                  type="text"
-                  className="btn btn-delete-prescription px-0 focus-none h-100"
-                  onClick={props.onClick}
-                >
-                  <i className="icon-Add" style={{ color: "white" }} />
-                </Button>
-              )}
-            </ZoomIn>
-          </div>
-          <div>
-            <span className="document-separator" />
-          </div>
-          <div
-            style={{ display: "flex", alignItems: "center", columnGap: "8px" }}
-          >
-            <Rotate direction={RotateDirection.Backward}>
-              {(props) => (
-                <Button
-                  type="text"
-                  className="btn btn-delete-prescription px-0 focus-none h-100"
-                  onClick={props.onClick}
-                >
-                  <img src={anticlockwise} alt="anticlockwise-direction" />
-                </Button>
-              )}
-            </Rotate>
-            <Rotate direction={RotateDirection.Forward}>
-              {(props) => (
-                <Button
-                  type="text"
-                  className="btn btn-delete-prescription px-0 focus-none h-100"
-                  onClick={props.onClick}
-                >
-                  <img src={clockwise} alt="clockwise-direction" />
-                </Button>
-              )}
-            </Rotate>
-          </div>
-        </div>
+        {isPdf ? <Controls /> : null}
 
         <div
           className="pdf-container"
@@ -228,24 +280,46 @@ const DocumentPreview = ({
             borderRadius: "10px",
           }}
         >
-          <Worker
-            workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}
-          >
-            <Viewer
-              fileUrl={cardData?.url}
-              plugins={[
-                zoomPluginInstance,
-                rotatePluginInstance,
-                pageNavigationPluginInstance,
-              ]}
-              renderTextLayer={false}
-              renderAnnotationLayer={false}
-              onDocumentLoad={onLoadSuccess}
-              onPageChange={onPageChange}
-              onZoom={handleOnZoom}
-              defaultScale={scale}
-            />
-          </Worker>
+          {cardData?.url?.includes(".pdf") ? (
+            <Worker
+              workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}
+            >
+              <Viewer
+                fileUrl={cardData?.url}
+                plugins={[
+                  zoomPluginInstance,
+                  rotatePluginInstance,
+                  pageNavigationPluginInstance,
+                ]}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+                onDocumentLoad={onLoadSuccess}
+                onPageChange={onPageChange}
+                onZoom={handleOnZoom}
+                defaultScale={scale}
+              />
+            </Worker>
+          ) : (
+            <TransformWrapper initialScale={1}>
+              {({ zoomIn, zoomOut, ...rest }) => (
+                <>
+                  <ImageControls />
+                  <TransformComponent>
+                    <img
+                      ref={imgRef}
+                      src={cardData?.url}
+                      alt="image-url"
+                      style={{
+                        transform: `rotate(${angle}deg)`,
+                        transition: "transform 0.5s",
+                        borderRadius: "12px",
+                      }}
+                    />
+                  </TransformComponent>
+                </>
+              )}
+            </TransformWrapper>
+          )}
         </div>
       </div>
     </div>
