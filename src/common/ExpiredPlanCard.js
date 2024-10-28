13fb50320d763ff3dba9c230d57c271a313d77e5
sharath-tatvacare <167509675+sharath-tatvacare@users.noreply.github.com>
2024-10-28 18:17:14 +0530

    Revert "updated styles of expiry card"

--- src/common/ExpiredPlanCard.js
+++ src/common/ExpiredPlanCard.js
@@ -40,7 +40,7 @@ const ExpiredPlanCard = () => {
                         <hr
                           style={{
                             borderTop: "1.5px dotted #00000024",
-                            margin: "13px",
+                            margin: "14px",
                           }}
                         />
                         <span className={styles.validity}>
