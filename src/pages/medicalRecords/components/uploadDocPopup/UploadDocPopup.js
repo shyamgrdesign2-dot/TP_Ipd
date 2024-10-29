1baa2f1224079499bd4b0d4478e3b2f91c06ca37
harish-tatvacare <harish.y@tatvacare.in>
2024-10-29 16:30:41 +0530

    version bump and removed hasCheckedFirebase

--- src/pages/medicalRecords/components/uploadDocPopup/UploadDocPopup.js
+++ src/pages/medicalRecords/components/uploadDocPopup/UploadDocPopup.js
@@ -28,7 +28,7 @@ const UploadDocPopup = ({
   setIsFileTypeError,
 }) => {
   const dispatch = useDispatch();
-  const { state, location } = useLocation();
+  const { state } = useLocation();
   const patient_data = state?.patient_data;
   const patientUniqueId =
     patient_data?.patient_unique_id || patientData?.patient_unique_id || 0;
@@ -81,10 +81,6 @@ const UploadDocPopup = ({
       setUploadDocDrawer(true);
     }
   };
-
-  useEffect(() => {
-    hasCheckedFirebase = false;
-  }, [location]);
 
   useEffect(() => {
     const checkInFireBase = async () => {
