660285ce0a6075ec90980a73269b2c13cd46546d
AkhilG-TatvaCare <akhil.goudar@tatvacare.in>
2024-10-30 13:19:49 +0530

    Opd-Plans QR UI fixe.

--- src/common/Header.js
+++ src/common/Header.js
@@ -507,10 +507,14 @@ function Header({ locationPath }) {
     dispatch(openModal());
   };
 
+  const handleShowQRCode = () => {
+    setQRCodeVisible(true);
+  };
+
   const getMenuItems = () => {
-    const items = [
+    const commonItems = [
       {
-        label:
+        label: (
           <>
             <div className="me-3">
               {profile?.um_image ? (
@@ -522,116 +526,93 @@ function Header({ locationPath }) {
                 />
               ) : planDetails?.currentPlanStatus === "PAID" ? (
                 <PremiumUser />
-              ) :
-                <div className='rounded-pill patientProfile patientProfile52 border'>{makeDefaultLogo(profile?.um_name)}</div>
-              }
+              ) : (
+                <div className="rounded-pill patientProfile patientProfile52 border">
+                  {makeDefaultLogo(profile?.um_name)}
+                </div>
+              )}
             </div>
             <div>
-              <div className="text-black titleprint">{(profile?.um_name)}</div>
-              <div className="title-common">{(profile?.um_contact)}</div>
+              <div className="text-black titleprint">{profile?.um_name}</div>
+              <div className="title-common">{profile?.um_contact}</div>
             </div>
           </>
-        ,
+        ),
         key: '0',
       },
+      { type: 'divider' },
       {
-        type: 'divider',
-      },
-      {
-        label:
+        label: (
           <a onClick={() => setUpWebsiteUrl(1)}>
-            <div className="title-common me-5 d-flex align-items-center"><i className="icon-profile me-3"></i>My Profile</div>
+            <div className="title-common me-5 d-flex align-items-center">
+              <i className="icon-profile me-3"></i>My Profile
+            </div>
             <i className="icon-right iconrotate180"></i>
-          </a>,
+          </a>
+        ),
         key: '2',
       },
       {
-        label:
+        label: (
           <a onClick={() => setUpWebsiteUrl(2)}>
-            <div className="title-common me-5 d-flex align-items-center"><i className="icon-group me-3"></i>{`${profile?.website_publish && profile?.publish_url ? 'Visit' : 'Setup'} My Website`}</div>
+            <div className="title-common me-5 d-flex align-items-center">
+              <i className="icon-group me-3"></i>
+              {profile?.website_publish && profile?.publish_url
+                ? 'Visit'
+                : 'Setup'}{' '}
+              My Website
+            </div>
             <i className="icon-right iconrotate180"></i>
-          </a>,
+          </a>
+        ),
         key: '3',
       },
       {
-        label:
+        label: (
           <a onClick={myAvailability}>
-            <div className="title-common me-5 d-flex align-items-center"><i className="icon-calendar me-3"></i>My Availability</div>
+            <div className="title-common me-5 d-flex align-items-center">
+              <i className="icon-calendar me-3"></i>My Availability
+            </div>
             <i className="icon-right iconrotate180"></i>
-          </a>,
+          </a>
+        ),
         key: '4',
       },
       {
-        label:
+        label: (
           <a onClick={accountSettings}>
-            <div className="title-common me-5 d-flex align-items-center"><i className="icon-setting me-3"></i>Account Setting</div>
-            <i className="icon-right iconrotate180"></i>
-          </a>,
-        key: '5',
-      },
-      {
-        label:
-          <a onClick={() => ["TRIAL","EXPIRED"].includes(planDetails?.currentPlanStatus) ? handleClick() : accountSettings()}>
             <div className="title-common me-5 d-flex align-items-center">
-              {["TRIAL","EXPIRED"].includes(planDetails?.currentPlanStatus) && <img loading="lazy" src={upgradeIcon} className="me-3" alt="" />}
-              {planDetails?.currentPlanStatus === "PAID" && <img loading="lazy" src={crownIcon} className="me-3" style={{filter: 'brightness(0%)'}} alt="" />}
-              {["TRIAL","EXPIRED"].includes(planDetails?.currentPlanStatus) ? "Upgrade Plan" : "Subscription"}
-              {["TRIAL","EXPIRED"].includes(planDetails?.currentPlanStatus) && <div className="gradientBackground d-flex">
-                <div className="demoModeIndicatorSmall bg-danger" />
-                <span className='demoModeLabel'>Demo mode</span>
-              </div>}
+              <i className="icon-setting me-3"></i>Account Setting
             </div>
             <i className="icon-right iconrotate180"></i>
-          </a>,
+          </a>
+        ),
+        key: '5',
+      },
+    ];
+  
+    const extraItems = [
+      {
+        label: (
+          <a onClick={handleShowQRCode}>
+            <div className="title-common me-5 d-flex align-items-center">
+              <img src={qrIcon} className="me-3" alt="QR Icon" />
+              OPD Plan QR
+            </div>
+            <i className="icon-right iconrotate180"></i>
+          </a>
+        ),
         key: '6',
       },
-      {
-        label:
-          <a onClick={handleShowQRCode}>
-            <div className="title-common me-5 d-flex align-items-center"><img src={qrIcon} className="me-3"></img>OPD Plan QR</div>
-            <i className="icon-right iconrotate180"></i>
-          </a>,
-        key: '7',
-      },
-      // {
-      //   label:
-      //     <a>
-      //       <div className="title-common me-5 d-flex align-items-center"><i className="icon-upgrade me-3"></i>Upgrade Plan</div>
-      //       <i className="icon-right iconrotate180"></i>
-      //     </a>,
-      //   key: '5',
-      // },
-      // {
-      //   label:
-      //     <a>
-      //       <div className="title-common me-5 d-flex align-items-center"><i className="icon-help me-3"></i>Help Center</div>
-      //       <i className="icon-right iconrotate180"></i>
-      //     </a>,
-      //   key: '6',
-      // },
-
-
-      // CSS Also comment
-      // {
-      //   type: 'divider',
-      // },
-      // {
-      //   label: <><i className="icon-exit me-2"></i> Log Out</>,
-      //   key: '8',
-      // },
     ];
-
-    // if (!tokenData?.admin || process.env.REACT_APP_ENV === "prod") {
-    if (!tokenData?.admin) {
-      return items.filter((item) => item.key !== "5");
-    } else {
-      return items;
-    }
-  };
-
-  // Handle opening the QR code modal
-  const handleShowQRCode = () => {
-    setQRCodeVisible(true);
+  
+    // Combine common items with opd based on isOpdPlansAccessableFromGB
+    const items = isOpdPlansAccessableFromGB
+      ? [...commonItems, ...extraItems]
+      : commonItems;
+  
+    // If not admin, filter out account setting item, else return all items
+    return !tokenData?.admin ? items.filter((item) => item.key !== "5") : items;
   };
 
   const showHideBackModal = useCallback(() => {
@@ -772,18 +753,18 @@ function Header({ locationPath }) {
                     Scan the QR to view & buy OPD plans
                   </div>
                 </div>
-                  <div className="d-flex align-items-center mt-2 justify-content-between gap-4 mt-4">
+                  <div className="d-flex align-items-center justify-content-between gap-4 mt-4">
                     <ButtonOPD
                       onClick={handlePrint}
                       className="btn btn-primary1 btn-41 align-items-center d-flex justify-content-center"
-                      style={{width: "15rem",height: "3.3rem"}}
+                      style={{width: "13rem",height: "3rem"}}
                     >
                       <span className="fs-18 align-items-center d-flex "><i className="icon-Print me-2"></i>Print</span>
                     </ButtonOPD>
                     <ButtonOPD
                       onClick={handleDownload}
                       className="btn btn-primary1 btn-41 align-items-center d-flex justify-content-center"
-                      style={{width: "15rem",height: "3.3rem"}}
+                      style={{width: "13rem",height: "3rem"}}
                     >
                       <span className="fs-18 align-items-center d-flex"><i className="icon-download me-2"></i>Download</span>
                     </ButtonOPD>
