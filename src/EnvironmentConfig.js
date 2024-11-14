ee5fc4a38385d69d30feb9163f00beb0ce62bcf8
harish-tatvacare <harish.y@tatvacare.in>
2024-11-14 11:52:55 +0530

    updated opd encrypted url

--- src/EnvironmentConfig.js
+++ src/EnvironmentConfig.js
@@ -179,7 +179,7 @@ const prod = {
   razorpay_integration_url: "https://pm-razorpay-uat-integration.tatvacare.in",
   api_secret_key: "194627f7-u6hu-4378-q0e2-a34t5lt77f31",
   api_key: "TC_12o89l11327dfrt55",
-  opd_encryption_url:"https://api-prod.mytatva.in/api/v8/helper/encrypt_visit_data",
+  opd_encryption_url:"https://api.mytatva.in/api/v8/helper/encrypt_visit_data",
   upload_doc_api_url: "https://pm-patient-docs-prod.tatvacare.in/"
 };
 
