51bcaff7d9845f1345cc67e10b02356e094b0512
care-hemantm <ex.hemantm@tatvacare.in>
2025-01-21 15:38:57 +0530

    added lite api key and secrets in uat and prod env.

--- src/EnvironmentConfig.js
+++ src/EnvironmentConfig.js
@@ -225,6 +225,10 @@ const uat = {
   bulk_messages: "https://pm-communication-uat.tatvacare.in",
   searchengine_url: "https://pm-searchengine-uat.tatvacare.in",
   razorPay_key: "rzp_test_PoLtBba5t8qJIm",
+  tatvaAi_api_url: "https://pm-doctor-tatva-uat.tatvacare.in",
+  tatvaAi_url: "https://tatva-ai-uat.tatvacare.in",
+  lite_api_key: "68ceb696-0b60-47e8-9deb-b0e359d93d2f",
+  lite_secret_key: "4b301416-946d-4b22-a083-69111ce79245",
   gen_rx_api_url: "https://pm-rxdigitization-uat.tatvacare.in",
 };
 
@@ -276,6 +280,10 @@ const prod = {
   bulk_messages: "https://pm-communication-prod.tatvacare.in",
   searchengine_url: "https://pm-searchengine-prod.tatvacare.in",
   razorPay_key: "rzp_live_GvLkPHHNSMs2G3",
+  tatvaAi_api_url: "https://pm-doctor-tatva-prod.tatvacare.in",
+  tatvaAi_url: "https://tatva-ai-prod.tatvacare.in",
+  lite_api_key: "68ceb696-0b60-47e8-9deb-b0e359d93d2f",
+  lite_secret_key: "4b301416-946d-4b22-a083-69111ce79245",
   gen_rx_api_url: "https://pm-rxdigitization-prod.tatvacare.in",
 };
 
