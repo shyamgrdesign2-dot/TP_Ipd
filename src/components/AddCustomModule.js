8fc30e2389145317ac5f4fd3585e15639639d7cf
sharath-tatvacare <sharath.m@tatvacare.in>
2025-02-18 17:01:07 +0530

    updated count in disabled condition

--- src/components/AddCustomModule.js
+++ src/components/AddCustomModule.js
@@ -281,7 +281,7 @@ const AddCustomModule = () => {
           type="link"
           icon={<PlusOutlined />}
           className="add-custom-module-link"
-          disabled={customModules.length >= 5}
+          disabled={customModules.length >= 10}
         >
           Add Custom Module
         </Button>
