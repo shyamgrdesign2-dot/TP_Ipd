493c272387766f18c5c25cdafec30e433527fb14
Akhil Goudar <akhilgoudar@Akhils-Laptop.local>
2025-02-25 13:25:10 +0530

    Setup - AddApoointment files strcture.

---
+++ src/pages/addAppointment/components/addAppointment.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function addAppointment() {
+  return (
+    <div>
+        <div className="add-appointment-container">
+            <h1>Add Appointment</h1>
+        </div>
+    </div>
+  )
+}
+
+export default addAppointment
