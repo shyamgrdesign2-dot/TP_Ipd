b3d26cbfbc25f590c7e49458af7c009de28940c2
Ruchi Verma <ruchi.verma@tatvacare.in>
2025-10-06 02:22:10 +0530

    add condition for user comming from Medeco to practice offering page

--- src/App.js
+++ src/App.js
@@ -226,13 +226,15 @@ function App() {
       if (!isReceptionist) {
         params.delete("authToken");
         // Navigate to appointment list
-        navigate(
-          {
-            pathname: "/",
-            search: params.toString(),
-          },
-          { replace: true }
-        );
+        // add condition for user comming from Medeco to practice offering page
+        if (location.pathname !== "/our-offerings")
+          navigate(
+            {
+              pathname: "/",
+              search: params.toString(),
+            },
+            { replace: true }
+          );
       }
     }
   }, [authToken, setToken, navigate]);
