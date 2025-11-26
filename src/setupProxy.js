080a606d28a1b2ec10932154e79dd8a75c08fa16
IntelKancharala <kancharla.intel@tatvacare.in>
2025-11-26 18:49:24 +0530

    fix - for using prod branch of remote repo instead of ipd-dev in this ipd-prod main repo - 2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,44 +2,44 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   // Proxy shared-ui remote to avoid CORS in local dev when testing IPD env
-  if (process.env.REACT_APP_ENV === 'ipd') {
-    app.use(
-      '/shared-ui',
-      createProxyMiddleware({
-        target:
-          'https://pm-storybook-ui-uat.kindmushroom-6f77b425.centralindia.azurecontainerapps.io',
-                  changeOrigin: true,
-        secure: true,
-        ws: false,
-        pathRewrite: {
-          '^/shared-ui': '/',
-        },
-        headers: {
-          Host:
-            'pm-storybook-ui-uat.kindmushroom-6f77b425.centralindia.azurecontainerapps.io',
-        },
-      })
-    );
-  } else if (process.env.REACT_APP_ENV === 'prod') {
-    app.use(
-      '/shared-ui',
-      createProxyMiddleware({
-        target:
-          // 'https://pm-storybook-ui-uat.kindmushroom-6f77b425.centralindia.azurecontainerapps.io',
-          "https://pm-storybook-ui-prod.tatvacare.in",
-        changeOrigin: true,
-        secure: true,
-        ws: false,
-        pathRewrite: {
-          '^/shared-ui': '/',
-        },
-        headers: {
-          Host:
-            'pm-storybook-ui-prod.tatvacare.in',
-        },
-      })
-    );
-  }
+  // if (process.env.REACT_APP_ENV === 'ipd') {
+  //   app.use(
+  //     '/shared-ui',
+  //     createProxyMiddleware({
+  //       target:
+  //         'https://pm-storybook-ui-uat.kindmushroom-6f77b425.centralindia.azurecontainerapps.io',
+  //                 changeOrigin: true,
+  //       secure: true,
+  //       ws: false,
+  //       pathRewrite: {
+  //         '^/shared-ui': '/',
+  //       },
+  //       headers: {
+  //         Host:
+  //           'pm-storybook-ui-uat.kindmushroom-6f77b425.centralindia.azurecontainerapps.io',
+  //       },
+  //     })
+  //   );
+  // } else if (process.env.REACT_APP_ENV === 'prod') {
+  // }
+  app.use(
+    '/shared-ui',
+    createProxyMiddleware({
+      target:
+        // 'https://pm-storybook-ui-uat.kindmushroom-6f77b425.centralindia.azurecontainerapps.io',
+        "https://pm-storybook-ui-prod.tatvacare.in",
+      changeOrigin: true,
+      secure: true,
+      ws: false,
+      pathRewrite: {
+        '^/shared-ui': '/',
+      },
+      headers: {
+        Host:
+          'pm-storybook-ui-prod.tatvacare.in',
+      },
+    })
+  );
 };
 
 
